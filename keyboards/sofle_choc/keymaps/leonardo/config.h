0455ec38e977c84fe8adc8e46e0896b8cd8f96b5
Leo Antuna <leoantuna@hotmail.com>
2025-12-04 21:50:14 -0500

    modificacion en las pantallas oled

--- keyboards/sofle_choc/keymaps/leonardo/config.h
+++ keyboards/sofle_choc/keymaps/leonardo/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Apaga OLED después de 60 segundos sin teclear
-#define OLED_TIMEOUT 60000
+#define OLED_TIMEOUT 6000000
 
 // No necesitamos WPM súper alto
 #define WPM_LAUNCH_CONTROL
--- keyboards/sofle_choc/keymaps/leonardo/rules.mk
+++ keyboards/sofle_choc/keymaps/leonardo/rules.mk
@@ -1,2 +1,19 @@
 OLED_ENABLE = yes
-WPM_ENABLE  = yes
+WPM_ENABLE  = yes        # Ocean usa WPM, lo dejamos por ahora
+
+# Solo esta animación
+SRC += ocean_dream.c
+# SRC += luna.c
+# SRC += bongocat.c
+
+# Optimizaciones de tamaño
+ LTO_ENABLE        = yes
+CONSOLE_ENABLE    = no
+COMMAND_ENABLE    = no
+MOUSEKEY_ENABLE   = no
+BOOTMAGIC_ENABLE  = no
+MIDI_ENABLE       = no
+AUDIO_ENABLE      = no
+STENO_ENABLE      = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE      = no
